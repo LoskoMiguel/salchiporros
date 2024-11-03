80b163a58a209fee5c40726aecb20f129a897a33
LoskoMiguel <magelposada@gmail.com>
2024-11-03 00:34:45 -0500

    agregue el dotenv

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
 from fastapi import FastAPI
-import requests
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import psycopg2
+import os
+
+load_dotenv()
 
 app = FastAPI()
+
+# Conexión a la base de datos de Supabase
+connection = psycopg2.connect(
+    host=os.getenv("host"),
+    port=os.getenv("port"),
+    dbname=os.getenv("dbname"),
+    user=os.getenv("user"),
+    password=os.getenv("password")
+)
+cursor = connection.cursor()
+
+# Comando SQL para crear la tabla
+create_table_sql = """
+CREATE TABLE IF NOT EXISTS usuarios (
+    id SERIAL PRIMARY KEY,
+    full_name TEXT NOT NULL,
+    email varchar(250),
+    password text,
+    dni text,
+    numero_cuenta text,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+"""
+cursor.execute(create_table_sql)
+connection.commit()
+
+print("Tabla creada con éxito")
+
+# Cierra la conexión
+cursor.close()
+connection.close()
 
 @app.get("/")
 class user(BaseModel):
@@ -11,4 +46,3 @@ class user(BaseModel):
     dni: str
     password: str
     confirm_password: str
-
