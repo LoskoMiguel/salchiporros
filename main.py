7664d0663fc2878a18edc5b72a75543396f1b1c6
LoskoMiguel <magelposada@gmail.com>
2024-11-03 00:55:14 -0500

    agregrue nuevas funcionaliade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from dotenv import load_dotenv
 import psycopg2
+import bcrypt
 import os
 
 load_dotenv()
@@ -9,13 +10,15 @@ load_dotenv()
 app = FastAPI()
 
 # Conexión a la base de datos de Supabase
-connection = psycopg2.connect(
-    host=os.getenv("host"),
-    port=os.getenv("port"),
-    dbname=os.getenv("dbname"),
-    user=os.getenv("user"),
-    password=os.getenv("password")
-)
+def get_db_connection():
+    return psycopg2.connect(
+        host=os.getenv("host"),
+        port=os.getenv("port"),
+        dbname=os.getenv("dbname"),
+        user=os.getenv("user"),
+        password=os.getenv("password")
+    )
+connection = get_db_connection()
 cursor = connection.cursor()
 
 # Comando SQL para crear la tabla
@@ -45,4 +48,28 @@ class user(BaseModel):
     email: str
     dni: str
     password: str
-    confirm_password: str
+
+@app.post("/register")
+async def register_user(user: user):
+    if user.password != user.confirm_password:
+        raise HTTPException(status_code=400, detail="Las contraseñas no coinciden.")
+    
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
+    
+    connection = get_db_connection()
+    cursor = connection.cursor()
+
+    insert_sql = """
+    INSERT INTO usuarios (full_name, email, password, dni, numero_cuenta) VALUES (%s, %s, %s, %s, %s)
+    RETURNING id;
+    """
+    
+    try:
+        cursor.execute(insert_sql, (user.fullname, user.email, user.password, user.dni))
+        user_id = cursor.fetchone()[0]
+        connection.commit()
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    finally:
+        cursor.close()
+        connection.close()
